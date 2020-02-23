d0dd1bfc12a1f70e3b775992ca1c1f6ab03b5522
josemasri <josemasri2@gmail.com>
2020-02-22 19:58:34 -0600

    Added configs heroku

--- src/communications/mysql.ts
+++ src/communications/mysql.ts
@@ -1,18 +1,19 @@
+import { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } from '../config/config';
 import mysql = require('mysql');
 
 export default class MySQL {
 
     private static _instance: MySQL;
     private _conn: mysql.Connection;
-    private host = 'localhost';
-    private user = 'root';
-    private password = '';
-    private database = 'petly';
+    private host = DB_HOST;
+    private user = DB_USERNAME;
+    private password = DB_PASSWORD;
+    private database = DB_NAME;
 
     
     private constructor() {
         this._conn = mysql.createConnection({
-            host: this.host,
+            host: `https://${this.host}`,
             user: this.user,
             password: this.password,
             database: this.database
