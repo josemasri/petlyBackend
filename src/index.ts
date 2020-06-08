7ec051fc279e046ac878178a4e80a16905ac1c50
Jose Masri <josemasri2@gmail.com>
2020-06-08 13:50:55 -0500

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
+/** index.ts
+ * Este es el archivo inicial de la aplicación
+ * y sirve para configurar los controlladores así
+ * como realizar configuraciones iniciales
+ */
+
 import cors from "cors";
 
+import Server from "./server/server";
+import routerInformacion from "./controller/routerInformacion";
+import bodyParser from "body-parser";
+import routerCuentas from "./controller/routerCuentas";
+import routerDonaciones from "./controller/routerDonaciones";
+import fileUpload from "express-fileupload";
+import routerAnimales from "./controller/routerAnimales";
 
-import Server from './server/server';
-import routerInformacion from './controller/routerInformacion';
-import bodyParser from 'body-parser';
-import routerCuentas from './controller/routerCuentas';
-import routerDonaciones from './controller/routerDonaciones';
-import fileUpload from 'express-fileupload';
-import routerAnimales from './controller/routerAnimales';
-
+// Instanciando la clase Server
 const server = Server.instance;
-// Obrener atributos del body
+// Permitiendo a cualquier dominio hacer peticiones a este servidor
 server.app.use(cors());
+// Configuracion para subir archivos
 server.app.use(fileUpload());
+// Obrener atributos del body
 server.app.use(bodyParser.urlencoded({ extended: false }));
 server.app.use(bodyParser.json());
 
+// Configuracion de controladores
+server.app.use("/informacion", routerInformacion);
+server.app.use("/cuentas", routerCuentas);
+server.app.use("/donaciones", routerDonaciones);
+server.app.use("/animales", routerAnimales);
 
-server.app.use('/informacion', routerInformacion);
-server.app.use('/cuentas', routerCuentas);
-server.app.use('/donaciones', routerDonaciones);
-server.app.use('/animales', routerAnimales);
-
+// Inicio del servidor
 server.start(() => {
-    console.log(`Server running on port ${server.port}`);
+  console.log(`Server running on port ${server.port}`);
 });
